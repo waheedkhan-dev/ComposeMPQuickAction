d8bbbf51500c92bb43a1006e87438fde1ada4ed3
Waheed Khan <waheedkhan6446@gmail.com>
2025-05-13 11:13:55 +0500

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/org/wk/composempquickaction/App.kt
@@ -0,0 +1,20 @@
+package org.wk.composempquickaction
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text(text = "Hello World!")
+        }
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/org/wk/composempquickaction/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.wk.composempquickaction
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
